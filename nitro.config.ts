5e0efcebae17e4124b356aedf016ea53ffabf8a6
ruan-cat <1219043956@qq.com>
2025-09-13 01:50:00 +0800

    ⚙️ config: 配置默认的打包命令为 vercel 平台

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,7 +2,8 @@ import { defineNitroConfig } from "nitropack/config";
 
 // https://nitro.build/config
 export default defineNitroConfig({
-  preset: "vercel",
+  // 不推荐 应该在 nitro 的 build 命令内指定 preset
+  // preset: "vercel",
   compatibilityDate: "latest",
   srcDir: "server",
   imports: false,
