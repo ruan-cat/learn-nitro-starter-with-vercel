522957289b869494ce2a48019a9db0e49be76c0a
ruan-cat <1219043956@qq.com>
2026-02-26 23:36:13 +0800

    🔧 config(config)!: 配置兼容性日期，确保构建产物不包含符号链接目录。

--- nitro.config.ts
+++ nitro.config.ts
@@ -4,7 +4,14 @@ import { defineConfig } from "nitro";
 export default defineConfig({
 	// 不推荐 应该在 nitro 的 build 命令内指定 preset
 	// preset: "vercel",
-	compatibilityDate: "latest",
+
+	compatibilityDate: {
+		// https://v3.nitro.build/deploy/providers/cloudflare
+		cloudflare: "2024-09-19",
+		// https://nitro.build/deploy/providers/vercel#observability
+		vercel: "2024-09-19",
+	},
+
 	serverDir: "server",
 	imports: false,
 
